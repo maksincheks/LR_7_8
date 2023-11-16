b8dfe6f1cfa643eab109a5805ceb27828f510d41
Исупов Максим Олегович <144238218+maksincheks@users.noreply.github.com>
2023-11-16 10:34:52 +0300

    Delete зад2.pas

--- зад2.pas
+++
@@ -1,8 +0,0 @@
-﻿Program Zad2;
-var st: string; a,i: integer;
-begin
-readln (st);
-st := st + ', ' + st + ', ' + st;
-writeln ('Количество символов: ', length (st));
-writeln ('Строка: ', st);
-end.
